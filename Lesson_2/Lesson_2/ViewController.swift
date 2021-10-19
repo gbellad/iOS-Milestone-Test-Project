b3e513bae14bae88fdee97eaba9c04fbea7bb679
Gouri Bellad <gouribellad@Gouris-MacBook-Pro.local>
2021-10-19 02:08:15 -0400

    Lesson_2

---
+++ Lesson_2/Lesson_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_2
+//
+//  Created by Gouri Bellad on 10/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
