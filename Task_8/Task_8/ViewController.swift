1d4973333e7f215705b13e5cd98bc9f69d02a76d
Gouri Bellad <gouribellad@Gouris-MacBook-Pro.local>
2021-10-25 16:57:00 -0400

    Task8 work

---
+++ Task_8/Task_8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task_8
+//
+//  Created by Gouri Bellad on 10/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
